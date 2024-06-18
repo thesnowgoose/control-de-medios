5817b8e1d3283767b86ec347cba08f0267736009
Thesnowgoose <leonardo.carrasco@encora.com>
2024-06-17 20:17:19 -0600

    capitalize welcome name

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { logout } from '../services/authentication';
 
 export function Header({ name }) {
+    const username =  name.charAt(0).toUpperCase() + name.slice(1);
     return (
         <div id="header" className='d-flex flex-column mx-4 mb-3'>
             <div className='d-flex flex-row justify-content-between p-3 bg-white'>
@@ -10,7 +11,7 @@ export function Header({ name }) {
                 <img src={`${process.env.PUBLIC_URL}/gob-logo.png`} alt="logo" />
             </div>
             <div className='d-flex flex-row justify-content-between px-4 py-2 align-items-center bg-pearl'>
-                <h4>Bienvenid@ {name}</h4>
+                <h4>Bienvenid@ {username}</h4>
                 <button
                     className="login__btn"
                     onClick={logout}

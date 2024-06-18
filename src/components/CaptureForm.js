1a62fb0618566049713eacdee0673dda02b0f2d0
Thesnowgoose <leonardo.carrasco@encora.com>
2024-06-17 23:26:06 -0600

    display medios options on select

--- src/components/CaptureForm.js
+++ src/components/CaptureForm.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-export function CaptureForm() {
+export function CaptureForm({ medios }) {
+
     return (
         <div id="capture__form" className='d-flex justify-content-between py-4 mx-4'>
             <div className='d-flex justify-content-between mr-4'>
-                <input type="text" placeholder='Seleccione Medio'/>
+                {/* <input type="text" placeholder='Seleccione Medio'/> */}
+                <select name="medios" id="medios-select">
+                    <option value="" selected disabled>Seleccione Medio</option>
+                    { medios.map((medio, index) =>  <option key={index} value={medio.id}>{medio.code}</option>) }
+                </select>
                 {/* <input type="text" placeholder='Fecha' readOnly/>
                 <input type="text" placeholder='Hora' readOnly/> */}
                 {/* <input type="text" placeholder='Creado Por'/> */}

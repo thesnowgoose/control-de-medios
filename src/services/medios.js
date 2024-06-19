d5b75d4c4a02b9dd09cf92987df1132f9d56ac19
Thesnowgoose <leonardo.carrasco@encora.com>
2024-06-19 13:51:43 -0600

    comment code, add order and limit

--- src/services/medios.js
+++ src/services/medios.js
@@ -1,30 +1,40 @@
 import moment from 'moment';
-import { query, getDocs, collection, addDoc, doc, updateDoc} from "firebase/firestore";
+import { query, getDocs, collection, addDoc, doc, updateDoc, orderBy, limit } from "firebase/firestore";
 import { db } from '../firebase';
 
-const MEDIO_TYPES = ["MET", "XLD", "VB", "HK", "Regan Lowe", "Lauril (S)", "SB", "Sb"  ];
+// const MEDIO_TYPES = ["MET", "XLD", "VB", "HK", "Regan Lowe", "Lauril (S)", "SB", "Sb"  ];
 
-const addMediosTypes = async () => {
-    try {
-        MEDIO_TYPES.forEach(async code => {
-            await addDoc(collection(db, "medios"), {
-                code ,
-            });
-        });
-    } catch (error) {
-        console.log({error});
-    }
-}
+// const MEDIO_TYPES = [
+//  "A.Arginina", "A.DNA", "A.DNAse", "A.Movilidad", "A.Neutralizante", "A.Nutritivo", "A.Purificado", "A.Sangre", "A.T1N1",
+//  "A.Urea", "A.Urea+NaCl", "APA", "APA Doble", "APA 0.1%", "APM", "Astel", "BAB", "Biggy", "BG", "BHI", "BP", "BRV", "C. Arginina",
+//  "CEC", "Cefalexina", "Citrato", "CCC", "CGPB", "Charcoal", "C.Mucato", "C.Neutralizante", "CPA", "CPA 1%", "C. Sorbitol", "CST",
+//  "CST+Fe", "CST+K2SO3", "CVB", "D-Fraser", "EMB", "EMB-L", "EMJH", "Fosfatos", "Fraser", "GHL", "Lauril (s)", "Lauril (1.5)", "Lauril (3)",
+//  "Lauril-Mug", "LIA", "LIA+NaCl", "Malonato", "Métodos", "Mck/XLD", "Mck", "Mck-S", "MIO", "MIO+NaCL", "MMGB", "MRVP", "Mkttn", "OXA",
+//  "Palcam", "PBS", "PDA", "PM", "RVS", "SSP", "SM", "Solución salina 0.85%", "TBX", "T1N0", "T1N1", "T1N3", "T1N6",
+//  "T1N8", "T1N10", "T1N12", "TCBS", "Tetra", "Triples", "TSA", "TSA+NaCl", "TSI", "TSI+NacL", "VB/SB"
+// ];
+
+// const addMediosTypes = async () => {
+//     try {
+//         MEDIO_TYPES.forEach(async code => {
+//             await addDoc(collection(db, "medios"), {
+//                 code ,
+//             });
+//         });
+//     } catch (error) {
+//         console.log({error});
+//     }
+// }
 
 export const readMediosTypes = async (setState) => {
-    const q = query(collection(db, "medios"));
+    const q = query(collection(db, "medios"),  orderBy("code"));
     const doc = await getDocs(q);
     const mediosTypes = doc.docs.map(medio => ({ id: medio.id, code: medio.data().code }));
     setState((prev) => ({ ...prev, mediosTypes }));
 }
 
 export async function readMediosRequests(setState) {
-    const q = query(collection(db, "mediosRequests"));
+    const q = query(collection(db, "mediosRequests"), limit(50));
     const doc = await getDocs(q);
     if (doc.docs.length === 0) return;
     const mediosRequests = doc.docs.map(buildMedio);

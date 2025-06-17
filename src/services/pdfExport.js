3eb01ce8b143782fefadaeee59827a8ca7a87cf2
Thesnowgoose <leonardo.carrasco@encora.com>
2025-06-16 22:47:24 -0600

    add library to export pdf, add function to process medios and create pdf

---
+++ src/services/pdfExport.js
@@ -0,0 +1,29 @@
+import jsPDF from "jspdf";
+import moment from "moment";
+import { autoTable } from 'jspdf-autotable';
+
+export function exportToPDF(records) {
+    records.forEach(el => {
+        delete el.id
+    });
+    debugger
+    const fileName = moment(new Date()).format('DD-MMM-yyyy');
+    const pdfFile = new jsPDF();
+
+    const columns = Object.keys(records[0]);
+    const rows = [];
+
+    records.forEach(element => {      
+        const temp = Object.values(element);
+        rows.push(temp);
+    });
+
+    autoTable(pdfFile, {
+        head: [columns],
+        body: rows,
+        startY: 60,
+        theme: "striped",
+    });
+
+    pdfFile.save(`${fileName}.pdf`);
+}
